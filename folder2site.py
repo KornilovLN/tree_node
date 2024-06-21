b4c1900bbecb44823bcb67ac47e02e20fced42f5
KornilovLN <ln.KornilovStar@gmail.com>
2024-06-21 19:05:07 +0300

    Edit program folder2site.py and stile.css

--- folder2site.py
+++ folder2site.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+
+
 def create_site_structure(src_dir, dest_dir):
     try:
         os.makedirs(dest_dir, exist_ok=True)
@@ -8,29 +10,34 @@ def create_site_structure(src_dir, dest_dir):
     except OSError as e:
         print(f"Ошибка при создании структуры сайта: {e}")
 
+
+
 def copy_directory(src_path, dest_path, dest_dir):
     for entry in os.listdir(src_path):
         src_entry_path = os.path.join(src_path, entry)
         dest_entry_path = os.path.join(dest_path, entry)
 
         if os.path.isdir(src_entry_path):
-            os.makedirs(dest_entry_path, exist_ok=True)
+            os.makedirs(dest_entry_path, exist_ok=True)            
             folder_name = os.path.basename(dest_entry_path)
             html_file_path = os.path.join(dest_entry_path, f"{folder_name}.html")
             parent_dir = os.path.dirname(dest_entry_path)
             parent_folder_name = os.path.basename(parent_dir)
+
             back_link = f"../{parent_folder_name}.html" if parent_folder_name else ""
             root_link = "../" * (dest_entry_path.count(os.sep) - dest_dir.count(os.sep)) + "sunpp_docs.html"
+
             next_folders = [f for f in os.listdir(src_entry_path) if os.path.isdir(os.path.join(src_entry_path, f))]
             next_links = [f"{folder_name}.html" for folder_name in next_folders]
+            
             create_html_file(dest_entry_path, folder_name, dest_dir, html_file_path, root_link, back_link, next_links)
+            
             context_dir = os.path.join(dest_entry_path, "context")
             os.makedirs(context_dir, exist_ok=True)
             copy_files(src_entry_path, context_dir)
             copy_directory(src_entry_path, dest_entry_path, dest_dir)
-        else:
-            # copy_files(src_path, dest_path)  не копировать файлы просто в подкаталоги
-            pass
+
+
 
 def copy_files(src_path, dest_path):
     for file_name in os.listdir(src_path):
@@ -40,15 +47,32 @@ def copy_files(src_path, dest_path):
             shutil.copy(src_file_path, dest_file_path)
 
 
-#def create_html_file(dest_path, html_file_path, folder_name, next_links, back_link, root_link, dest_dir):
+
 def create_html_file(dest_path, folder_name, dest_dir, html_file_path, root_link, back_link, next_links):
     level = dest_path.count(os.sep) - dest_dir.count(os.sep)
 
     style_link = "../" * level + "styles/style.css"
-
     icon_link = "../" * level + "icons/ivl.png"
-
     next_links_html = create_next_links(next_links, folder_name)
+
+    # Формируем полный путь к текущей папке
+    full_path = os.path.relpath(dest_path, dest_dir)
+
+    # Получаем список файлов в папке "context"
+    context_dir = os.path.join(dest_path, "context")
+    context_files = []
+    if os.path.exists(context_dir):
+        context_files = os.listdir(context_dir)
+
+    # Формируем HTML-код для отображения списка файлов
+    context_files_html = ""
+    if context_files:
+        context_files_html = "<div style='margin-left: 20px;'><h4>Файлы в папке context:</h4>\n"
+        for file in context_files:
+            context_files_html += f"<p>{file}</p>\n"
+        context_files_html += "</div>"
+
+    print(f"context_files_html: {context_files_html}")
 
     with open(html_file_path, "w", encoding="utf-8") as file:
         file.write(f"""<!DOCTYPE html>
@@ -85,9 +109,11 @@ def create_html_file(dest_path, folder_name, dest_dir, html_file_path, root_link
             <div class="main-content">
                 <div>
                     <h3>{folder_name}</h3>
+                    <p><i>{full_path}</i></p>
                     <div>
                         <!-- Тут ссылки на следующие подкаталоги (их может быть от 1-го до нескольких) -->
                          {next_links_html}
+                         {context_files_html}
                         <!-- Тут ссылка на предыдущий (Назад) -->
                         <!-- Тут ссылка в начало (На главную) -->
                     </div>
@@ -109,22 +135,14 @@ def create_html_file(dest_path, folder_name, dest_dir, html_file_path, root_link
 </body>
 </html>""")
 
-'''
-def create_next_links(next_links):
-    links_html = ""
-    for link in next_links:
-        links_html += f'<a href="{link}"><h3>{os.path.splitext(link)[0]}</h3></a><br>\n'
-    return links_html
-'''
+
+
 def create_next_links(next_links, folder_name):
     links_html = ""
     for link in next_links:
         link_folder = os.path.splitext(link)[0]
 
-        # links_html += f'<a href="{link_folder}/{link}"><h3>{link_folder}</h3></a><br>\n'
-
         if folder_name == ".":
-            # links_html += f'<a href="{link}"><h3>{link_folder}</h3></a><br>\n'
             links_html += f'<a href="{link_folder}/{link}"><h3>{link_folder}</h3></a><br>\n'
         else:
             links_html += f'<a href="{link_folder}/{link}"><h3>{link_folder}</h3></a><br>\n'
@@ -142,13 +160,12 @@ def main():
 
     # Создаем структуру сайта
     create_site_structure(src_dir, dest_dir)
-    '''
-    # Создаем файл sunpp_docs.html в корневой папке сайта
-    sunpp_docs_path = os.path.join(dest_dir, "sunpp_docs.html")
-    with open(sunpp_docs_path, "w", encoding="utf-8") as file:
-        next_folders = [f for f in os.listdir(src_dir) if os.path.isdir(os.path.join(src_dir, f))]
-        next_links = [f"{folder}.html" for folder in next_folders]
-    '''
+
+    # Создаем папку "content" в корневой папке сайта и копируем туда файлы из корневой папки исходного каталога
+    root_content_dir = os.path.join(dest_dir, "content")
+    os.makedirs(root_content_dir, exist_ok=True)
+    copy_files(src_dir, root_content_dir)
+
     # Создаем файл sunpp_docs.html в корневой папке сайта
     sunpp_docs_path = os.path.join(dest_dir, "sunpp_docs.html")
     with open(sunpp_docs_path, "w", encoding="utf-8") as file:
